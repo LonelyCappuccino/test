17671eaef9695830bdada71fcdfc1774e61afa6c
LonelyCappuccino <Golob@Klemens-Mac.local>
2013-09-13 14:50:04 +0200

    sad
    asfdbf

---
+++ test213/test213/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  test213
+//
+
+//  Copyright (c) 2013 Tomaz Golob. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ test213/test213/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  test213
+//
+
+//  Copyright (c) 2013 Tomaz Golob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
